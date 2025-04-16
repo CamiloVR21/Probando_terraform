459a64bfd47003419277d37d6f2b9cfab89f2044
CamiloVR21 <camilo12.vr@gmail.com>
2025-04-16 16:07:33 -0400

    Update: tags y outputs

--- main.tf
+++ main.tf
@@ -21,14 +21,32 @@ resource "aws_instance" "nginx-server" {
     vpc_security_group_ids = [
         aws_security_group.nginx-server-sg.id]
 
+    tags = {
+    Name= "nginx-server"
+    Enviroment= "test"
+    Owner= "camilo12.vr@gmail.com"
+    Team= "DevOps"
+    
+  }
+
 }
 #Se debe ejecutar esto para la KEY
 #ssh-keygen -t rsa -b 2048 -f "nginx-server.key"
+#Conectarse a la máquina con la clave publica
+#ssh ec2-user@44.214.119.173 -i .\nginx-server.key
 
 resource "aws_key_pair" "nginx-server-ssh" {
 
     key_name = "nginx-server-ssh"
     public_key = file("nginx-server.key.pub")
+
+    tags = {
+    Name= "nginx-server-ssh"
+    Enviroment= "test"
+    Owner= "camilo12.vr@gmail.com"
+    Team= "DevOps"
+    
+  }
   
 }
 
@@ -57,4 +75,26 @@ resource "aws_security_group" "nginx-server-sg" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+
+    tags = {
+    Name= "nginx-server-sg"
+    Enviroment= "test"
+    Owner= "camilo12.vr@gmail.com"
+    Team= "DevOps"
+    
+  }
+}
+
+output "server_public_ip" {
+
+    description = "DirecciónIP"
+    value = aws_instance.nginx-server.public_ip
+  
+}
+
+output "server_public_dns" {
+
+    description = "DirecciónIP"
+    value = aws_instance.nginx-server.public_dns
+  
 }
